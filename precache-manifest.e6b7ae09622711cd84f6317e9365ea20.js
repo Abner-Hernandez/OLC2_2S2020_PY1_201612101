f9007ac11bc609e5442fc2194973ad37232d34d3
Abner-Hernandez <abner18enero@gmail.com>
2020-08-29 17:06:24 -0600

    Updates

---
+++ precache-manifest.e6b7ae09622711cd84f6317e9365ea20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202825cc64803e76d9f11a3054df0292",
+    "url": "/OLC2_2S2020_PY1_201612101/index.html"
+  },
+  {
+    "revision": "cdf7ab724a05d9120072",
+    "url": "/OLC2_2S2020_PY1_201612101/static/css/2.2afc2e4b.chunk.css"
+  },
+  {
+    "revision": "87e2b855e99864edb475",
+    "url": "/OLC2_2S2020_PY1_201612101/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cdf7ab724a05d9120072",
+    "url": "/OLC2_2S2020_PY1_201612101/static/js/2.5b1e5b25.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/OLC2_2S2020_PY1_201612101/static/js/2.5b1e5b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e2b855e99864edb475",
+    "url": "/OLC2_2S2020_PY1_201612101/static/js/main.526f2cf7.chunk.js"
+  },
+  {
+    "revision": "dfbd4182722299f5d968",
+    "url": "/OLC2_2S2020_PY1_201612101/static/js/runtime-main.c72d9056.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/OLC2_2S2020_PY1_201612101/precache-manifest.e6b7ae09622711cd84f6317e9365ea20.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/OLC2_2S2020_PY1_201612101/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
