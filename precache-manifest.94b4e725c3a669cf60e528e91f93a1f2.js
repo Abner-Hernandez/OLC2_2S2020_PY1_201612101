091abd05536c08f092e98ec6eb8d4d9943b23e2e
Abner-Hernandez <abner18enero@gmail.com>
2020-09-06 20:52:24 -0600

    Updates

---
+++ precache-manifest.94b4e725c3a669cf60e528e91f93a1f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1862e183a55f0a2ee3dff2a34c6220b2",
+    "url": "/OLC2_2S2020_PY1_201612101/index.html"
+  },
+  {
+    "revision": "072cb2652badc82d222f",
+    "url": "/OLC2_2S2020_PY1_201612101/static/css/2.2afc2e4b.chunk.css"
+  },
+  {
+    "revision": "ec0aab71d54e3a2c06d7",
+    "url": "/OLC2_2S2020_PY1_201612101/static/css/main.1fc973a9.chunk.css"
+  },
+  {
+    "revision": "072cb2652badc82d222f",
+    "url": "/OLC2_2S2020_PY1_201612101/static/js/2.b7254e97.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/OLC2_2S2020_PY1_201612101/static/js/2.b7254e97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec0aab71d54e3a2c06d7",
+    "url": "/OLC2_2S2020_PY1_201612101/static/js/main.60757b32.chunk.js"
+  },
+  {
+    "revision": "dfbd4182722299f5d968",
+    "url": "/OLC2_2S2020_PY1_201612101/static/js/runtime-main.c72d9056.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/OLC2_2S2020_PY1_201612101/precache-manifest.b792b4158cd2155a0ed7f2f3a337bb85.js"
+  "/OLC2_2S2020_PY1_201612101/precache-manifest.94b4e725c3a669cf60e528e91f93a1f2.js"
 );
 
 self.addEventListener('message', (event) => {
